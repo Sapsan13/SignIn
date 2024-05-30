0c7e2c9effa809b23648ed497166e2acbd1a11ea
Sapsan13 <rd861k@gmail.com>
2024-05-30 10:47:00 +0300

    Swagger OrvalGenerate+Reactquery

--- src/app/swag/toursStatisticTotal/statistics/page.tsx
+++ src/app/swag/toursStatisticTotal/statistics/page.tsx
@@ -1,4 +1,16 @@
 "use client";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import styled from "styled-components";
 import { swagStatistics } from "@/store/actions/swaggerActions";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -21,11 +33,37 @@ const SwagStatistics = () => {
   // console.log("STATS");
   // console.log(stat);
 
+  const data: any = [];
+
   if (!stat) return null;
-  return stat.map((item: any) => (
-    <div key={item.name}>
-      <p>{item.name}</p> <p>{item.uv}</p> <p>{item.pv}</p> <p>{item.amt}</p>
-    </div>
-  ));
+  stat.map((item: any) =>
+    data.push({
+      key: item.name,
+      name: item.name,
+      uv: item.uv,
+      pv: item.pv,
+      atm: item.amt,
+    })
+  );
+  const SkeletontWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    height: 800px;
+  `;
+  console.log(data);
+  return (
+    <SkeletontWrapper>
+      <ResponsiveContainer width="100%" aspect={4.0 / 3.0}>
+        <BarChart width={150} height={10} data={data} barSize={8}>
+          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Bar dataKey="uv" fill="#009764" radius={[8, 8, 0, 0]} />
+          <Bar dataKey="pv" fill="#ffac82" radius={[8, 8, 0, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </SkeletontWrapper>
+  );
 };
+
 export default SwagStatistics;
--- src/app/swag/toursStatisticTotal/totIncome/page.tsx
+++ src/app/swag/toursStatisticTotal/totIncome/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import {
   swagStatistics,
   swagTotalIncome,
 } from "@/store/actions/swaggerActions";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 const SwagTotalIncome = () => {
   const router = useRouter();
   const dispatch = useDispatch();
@@ -24,11 +36,37 @@ const SwagTotalIncome = () => {
   // console.log("totalT");
   // console.log(total);
 
+  const data: any = [];
+
   if (!total) return null;
-  return total.map((item: any) => (
-    <div key={item.name}>
-      <p>{item.name}</p> <p>{item.uv}</p> <p>{item.pv}</p> <p>{item.amt}</p>
-    </div>
-  ));
+  total.map((item: any) =>
+    data.push({
+      key: item.name,
+      name: item.name,
+      uv: item.uv,
+      pv: item.pv,
+      atm: item.amt,
+    })
+  );
+  const SkeletontWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    height: 800px;
+    height: fit-content;
+  `;
+  console.log(data);
+  return (
+    <SkeletontWrapper>
+      <ResponsiveContainer width="100%" aspect={4.0 / 3.0}>
+        <BarChart width={150} height={10} data={data} barSize={8}>
+          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Bar dataKey="uv" fill="#009764" radius={[8, 8, 0, 0]} />
+          <Bar dataKey="pv" fill="#ffac82" radius={[8, 8, 0, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </SkeletontWrapper>
+  );
 };
 export default SwagTotalIncome;
